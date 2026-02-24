120f4a3fbb8598d2bfe89fb36aef71b172e75a66
georgebarrett <georgeaustinbarrett@gmail.com>
2026-02-24 18:22:05 +0000

    nextjs Link being used. vinyls added to navbar that takes users to the vinyls on sale

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
+import Link from 'next/link'
 import Logo from './components/logo'
-import './globals.css'
+import './styles/globals.css'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
@@ -8,24 +9,29 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             <body className="font-sans antialiased">
                 <header className="absolute top-0 left-0 w-full z-50 p-4">
                     <nav className="flex justify-between items-center px-8 py-6 text-white">
-                        <a
+                        <Link
                             href="/"
                             className="flex items-center text-4xl tracking-[0.2em] uppercase font-light"
                         >
                             Spinning Plates
                             <Logo />
-                        </a>
+                        </Link>
+
                         <div className="flex gap-10 text-2xl tracking-widest uppercase">
-                            <a href="/" className="relative-group">
+                            <Link href="/" className="relative group">
                                 Home
-                            </a>
-                            <a
-                                href="/about"
-                                className="relative-group"
-                            >
+                                <span className="absolute left-0 -bottom-1 h-px w-0 bg-white transition-all duration-300 group-hover:w-full" />
+                            </Link>
+
+                            <Link href="/about" className="relative group">
                                 About
                                 <span className="absolute left-0 -bottom-1 h-px w-0 bg-white transition-all duration-300 group-hover:w-full" />
-                            </a>
+                            </Link>
+
+                            <Link href="/products" className="relative group">
+                                Vinyl
+                                <span className="absolute left-0 -bottom-1 h-px w-0 bg-white transition-all duration-300 group-hover:w-full" />
+                            </Link>
                         </div>
                     </nav>
                 </header>
