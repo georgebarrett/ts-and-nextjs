35b1f107efd0c9f68f52af607f0c338717ee90c7
georgebarrett <georgeaustinbarrett@gmail.com>
2026-02-24 13:00:32 +0000

    metadata added to file for SEO. html img tage replaced with next.js Image

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+import Image from "next/image"
+
+export const metadata = {
+    title: 'Home | Spinning Plates',
+    description: 'Vinyl and Record shop'
+}
+
 export default function Home() {
     return (
         <main className="relative min-h-screen flex items-center justify-center text-white overflow-hidden">
             <div className="absolute inset-0 -z-10">
-                <img
+                <Image
                     src="/home.jpg"
                     alt="Vinyl records background"
-                    className="w-full h-full object-cover"
+                    fill
+                    priority
+                    className="object-cover"
                 />
                 <div className="absolute inset-0 bg-black/50" />
             </div>
